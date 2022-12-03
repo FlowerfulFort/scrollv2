5b4be7d8ad7063caf07477e8f927488158af4931
FlowerfulFort <skagyghk@naver.com>
2022-12-03 12:23:36 +0900

    RetrieveEvents by accountName

--- lib/DataQuery.dart
+++ lib/DataQuery.dart
@@ -1,6 +1,9 @@
+import 'dart:collection';
+
 import 'package:device_calendar/device_calendar.dart';
 import 'package:scrollv2/TaskObject.dart';
 import 'package:timezone/timezone.dart';
+import 'dart:developer' as dev;
 extension tzparse on TZDateTime {
   DateTime get toDateTime => DateTime(
     year, month, day, hour, minute, second
@@ -9,6 +12,7 @@ extension tzparse on TZDateTime {
 const Duration oneDay = Duration(days: 1);
 class DataQuery {
   static final DeviceCalendarPlugin _plugin = DeviceCalendarPlugin();
+  static final Map<String, List<String>> calMap = {};
   static bool isInitiated = false;
   static Future<void> init() async {
     final perm = await _plugin.hasPermissions();
@@ -16,6 +20,7 @@ class DataQuery {
       throw Exception('Perm Error.');
     }
     if(perm.data!) {  // if has permission.
+      _initMap();
       isInitiated = true;
       return;
     }
@@ -24,11 +29,22 @@ class DataQuery {
       throw Exception('Perm Error.');
     }
     if(afterReq.data!) {
+      _initMap();
       isInitiated = true;
       return;
     }
     // tail recursion.
     await init();
+  }
+  static Future<void> _initMap() async {
+    final cals = await _plugin.retrieveCalendars();
+    if(!cals.isSuccess) throw Exception('RetrieveCalendars Error');
+    for (var c in cals.data!) {
+      if (!calMap.containsKey(c.accountName!)) {
+        calMap[c.accountName!] = <String>[];
+      }
+      calMap[c.accountName!]?.add(c.id!);
+    }
   }
   static RetrieveEventsParams makeParam(DateTime fetchDate) {
     DateTime start = DateTime(fetchDate.year, fetchDate.month, fetchDate.day);
@@ -38,11 +54,17 @@ class DataQuery {
       endDate: end
     );
   }
-  static Future<List<TaskObject>> fetchEvents(String cal_id, DateTime fetchDate) async {
+  static Future<List<TaskObject>> fetchEvents(String cal_acc, DateTime fetchDate) async {
     if (!isInitiated) init();
-    final res = await _plugin.retrieveEvents(cal_id, makeParam(fetchDate));
-    if (!res.isSuccess) throw Exception('RetrieveEvents failed: $fetchDate');
-    final rawList = res.data!;
+    final rawList = <Event>[];
+    for (var c in calMap[cal_acc]!) {
+      final events = await _plugin.retrieveEvents(c, makeParam(fetchDate));
+      if (!events.isSuccess) dev.log('RetrieveEvents Error');
+      rawList.addAll(events.data!);
+    }
+    // final res = await _plugin.retrieveEvents(cal_acc, makeParam(fetchDate));
+    // if (!res.isSuccess) throw Exception('RetrieveEvents failed: $fetchDate');
+    // final rawList = res.data!;
     return rawList.map((event) {
       return TaskObject(event.title, event.start!.toDateTime, event.end!.toDateTime);
     }).toList();
