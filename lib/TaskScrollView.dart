782ac989e0afaf21861d90416a8d189673ea6403
FlowerfulFort <skagyghk@naver.com>
2022-12-04 23:23:56 +0900

    TaskScrollView pruned useless code

--- lib/TaskScrollView.dart
+++ lib/TaskScrollView.dart
@@ -9,7 +9,6 @@ import 'package:infinite_listview/infinite_listview.dart';
 import 'package:intl/intl.dart';
 import 'dart:developer' as dev;
 import 'package:scrollv2/TaskObject.dart';
-import 'package:scrollv2/DataQuery.dart';
 import 'package:scrollv2/DateCell.dart';
 class TaskScrollView extends StatefulWidget {
   final Function refreshCallBack;
@@ -42,47 +41,6 @@ class _TaskScrollViewState extends State<TaskScrollView> {
     return DefaultTabController(length: 1, child: _buildTab(widget._today));
   }
 
-  List<Widget> tasksTiles(var tasks) {
-    List<Widget> ret = [];
-    for (var task in tasks) {
-      ret.add(ElevatedButton(
-          onPressed: () {
-            // 일단 누르면 log에 일정정보 출력
-            dev.log('${task.title}: ${task.start} ~ ${task.end}');
-          },
-          style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
-              backgroundColor: Colors.lightBlue,
-              padding: const EdgeInsets.all(8)),
-          child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "${task.title}\n${DateFormat('a hh:mm', 'ko').format(task.start)} "
-                "~ ${DateFormat('a hh:mm', 'ko').format(task.end)}",
-              ))));
-      ret.add(const SizedBox(
-        height: 7,
-      ));
-    }
-    return ret;
-  }
-
-  Widget taskAddButton(DateTime date) {
-    return OutlinedButton(
-        onPressed: () async {
-          final data = widget.getData();
-          dev.log('title: ${data['title']}');
-          dev.log('startTime: ${data['startTime']}');
-          dev.log('endTime: ${data['endTime']}');
-        },
-        style: OutlinedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(17.0),),
-            side: BorderSide(width: 2.0, color: Colors.grey)
-            ),
-        child: const Icon(Icons.add, size: 20, color: Colors.grey));
-  }
-
   Widget _buildTab(DateTime today) {
     return InfiniteListView.separated(
       controller: _infiniteController,
@@ -106,17 +64,6 @@ class _TaskScrollViewState extends State<TaskScrollView> {
             ),
             Expanded(
               child: DateCell(widget.cal_id, targetDate, widget.getData),
-              // child: dateCell(targetDate),
-              // child: Column(
-              //   children: [
-              //     const SizedBox(height: 7),
-              //     ...tasksTiles(widget._tasks),
-              //     Container(
-              //         alignment: Alignment.center,
-              //         child: taskAddButton(targetDate)),
-              //     const SizedBox(height: 4),
-              //   ],
-              // ),
             ),
             const SizedBox(width: 15)
           ],
@@ -125,30 +72,6 @@ class _TaskScrollViewState extends State<TaskScrollView> {
       separatorBuilder: (BuildContext context, int index) =>
           const Divider(thickness: 1.2, height: 1.5, color: Color(0xff999999)),
       anchor: 0.0,
-    );
-  }
-
-  FutureBuilder dateCell(DateTime target) {
-    return FutureBuilder(
-      future: DataQuery.fetchEvents(widget.cal_id, target),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (!snapshot.hasData) {
-          return Container();
-        } else if (snapshot.hasError) {
-          throw Exception("Fetch Error");
-        } else {
-          return Column(
-            children: [
-              const SizedBox(height: 7),
-              ...tasksTiles(snapshot.data),
-              Container(
-                  alignment: Alignment.center,
-                  child: taskAddButton(target)),
-              const SizedBox(height: 4),
-            ],
-          );
-        }
-      }
     );
   }
 }
